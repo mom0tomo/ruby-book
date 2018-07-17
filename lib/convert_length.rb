f133b51f6dc2def93e61aa7ee127185fddf14428
mom0tomo <momo76w@gmail.com>
2018-07-17 18:25:53 +0900

    create convert_length

---
+++ lib/convert_length.rb
@@ -0,0 +1,12 @@
+class ConvertLength
+  def show_convert_length(length, unit_from, unit_to)
+    units = {
+        'm' => 1.0,
+        'ft' => 3.28,
+        'in' => 39.37
+    }
+
+    # 端数は小数第3位で四捨五入する
+    (length / units[unit_from] * units[unit_to]).round(2)
+  end
+end
---
+++ spec/lib/convert_length_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'convert_length'
+
+describe '#show_convert_length' do
+  let(:converted_length) { ConvertLength.new }
+
+  context '1メートルをインチに変換する場合' do
+
+    it '39.37であること' do
+      expect(converted_length.show_convert_length(1, 'm', 'in')).to eq 39.37
+    end
+  end
+
+  # context '15インチをメートルに変換する場合' do
+  #
+  #   it '0.38であること' do
+  #     expect(converted_length.show_convert_length).to eq '0.38'
+  #   end
+  # end
+end
