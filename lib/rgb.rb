8bffe070b1be66030f58a89235a0be6f8ef358c6
mom0tomo <momo76w@gmail.com>
2018-07-17 17:28:00 +0900

    refactor

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,14 +1,14 @@
 class Rgb
-  def to_hex(r, g, b)
+  # 10進数の3つの数値を16進数の文字列に変換する
+      def to_hex(r, g, b)
     [r, g, b].inject('#') do |hex, n|
       hex + n.to_s(16).rjust(2, '0')
     end
   end
 
+  # 16進数の文字列を10進数の数値3つに変換する
   def to_ints(hex)
-    r = hex[1..2]
-    g = hex[3..4]
-    b = hex[5..6]
+    r, g, b = hex[1..2], hex[3..4], hex[5..6]
 
     [r, g, b].map do |s|
       s.hex
