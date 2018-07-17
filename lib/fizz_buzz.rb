58888fcc0abdeccad3cfcc322d113681a4c2525c
mom0tomo <momo76w@gmail.com>
2018-07-17 15:30:37 +0900

    add spec

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,17 +1,19 @@
-def fizz_buzz(n)
-  if n%15==0
-    'FizzBuzz'
-  elsif n%3==0
-    'Fizz'
-  elsif n%5==0
-    'Buzz'
-  else
-    n.to_s
+class FizzBuzz
+  def print_fizz_buzz(n)
+    if n%15==0
+      'FizzBuzz'
+    elsif n%3==0
+      'Fizz'
+    elsif n%5==0
+      'Buzz'
+    else
+      n.to_s
+    end
   end
-end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(5)
-puts fizz_buzz(15)
+  # puts fizz_buzz(1)
+  # puts fizz_buzz(2)
+  # puts fizz_buzz(3)
+  # puts fizz_buzz(5)
+  # puts fizz_buzz(15)
+end
---
+++ spec/lib/fizz_buzz_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require "fizz_buzz"
+
+describe '#ptint_fizz_buzz' do
+  let(:fizz_buzz) { FizzBuzz.new }
+
+  context 'n が3と5の倍数の場合' do
+    let(:print_fizz_buzz) { fizz_buzz.print_fizz_buzz(15) }
+
+    it 'FizzBuzzを表示すること' do
+      expect(print_fizz_buzz).to eq 'FizzBuzz'
+    end
+  end
+
+  context 'nが3の倍数の場合' do
+    let(:print_fizz_buzz) { fizz_buzz.print_fizz_buzz(3) }
+
+    it 'Fizzを表示すること' do
+      expect(print_fizz_buzz).to eq 'Fizz'
+    end
+  end
+
+  context 'nが5の倍数の場合' do
+    let(:print_fizz_buzz) { fizz_buzz.print_fizz_buzz(5) }
+
+    it 'Buzzを表示すること' do
+      expect(print_fizz_buzz).to eq 'Buzz'
+    end
+  end
+
+  context 'nが3の倍数でも5の倍数でもない場合' do
+    let(:print_fizz_buzz) { fizz_buzz.print_fizz_buzz(1) }
+
+    it 'nを表示すること' do
+      expect(print_fizz_buzz).to eq '1'
+    end
+  end
+end
